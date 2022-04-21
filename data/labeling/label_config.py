8ac9fbf7d80ec5e3f433f05b385befe8912aefc5
Elena Semerova <62423312+Elena-Semerova@users.noreply.github.com>
2022-04-21 14:31:08 +0300

    create label_config.py

---
+++ data/labeling/label_config.py
@@ -0,0 +1,17 @@
+CANCEL_CULTURE_PATH = 'data/cancel_culture_clean.csv'
+FEMINISM_PATH = 'data/feminism_clean.csv'
+LGBTQ_PLUS_PATH = 'data/lgbtq_plus_clean.csv'
+AGEISM_PATH = 'data/ageism_clean.csv'
+LOOKISM_PATH = 'data/lookism_clean.csv'
+
+TOPICS = ['cancel_culture',
+          'feminism',
+          'lgbtq_plus',
+          'ageism',
+          'lookism']
+
+TOPICS_RU = {TOPICS[0]: 'культура отмены',
+             TOPICS[1]: 'феминизм',
+             TOPICS[2]: 'ЛГБТК+',
+             TOPICS[3]: 'эйджизм',
+             TOPICS[4]: 'лукизм'}
