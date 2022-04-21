d7655b8c3e6af7c8a4eecf3d8947757727c316c8
Elena Semerova <62423312+Elena-Semerova@users.noreply.github.com>
2022-04-21 14:32:15 +0300

    create make_dataset.py

---
+++ data/labeling/make_dataset.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+def read_data(topic_name, path):
+    data = pd.read_csv(path)
+    data['topic'] = topic_name
+    
+    return data[['content', 'topic']]
+
+def make_dataset(topics, topics_ru, shuffle=False, save=False):
+    data = pd.DataFrame({'content':[], 'topic':[]})
+    for topic in topics:
+        path = topic.upper() + '_PATH'
+        topic_name = topics_ru[topic]
+        data_i = read_data(topic_name, globals()[path])
+        data = pd.concat([data, data_i], ignore_index=True)
+        
+    if shuffle:
+        data = data.sample(frac=1).reset_index(drop=True)
+    
+    if save:
+        data.to_csv('data/data_for_labeling.csv', index=False)
+    
+    return data
